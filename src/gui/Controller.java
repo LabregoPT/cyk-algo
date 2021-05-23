6652d5581e9c937f9d29948c5eaa15ea0b9575ee
ZornStolz <juan.vera1@correo.icesi.edu.co>
2021-05-23 00:24:49 -0500

    interface drawn

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,57 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField txtFieldVariables;
+
+    @FXML
+    private Button butContinuarCadena;
+
+    @FXML
+    private Button butContinueVariables;
+
+    @FXML
+    private ComboBox<?> comboBoxVariable;
+
+    @FXML
+    private TextField txtFieldProduction;
+
+    @FXML
+    private Button butAddProduction;
+
+    @FXML
+    private Button butEvaluar;
+
+    @FXML
+    private TextField txtCadena;
+
+    @FXML
+    private ComboBox<?> comboBoxTipoRegla;
+
+    @FXML
+    void addProduction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void continuarCadena(ActionEvent event) {
+
+    }
+
+    @FXML
+    void evaluarCadena(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getVariables(ActionEvent event) {
+
+    }
 }
