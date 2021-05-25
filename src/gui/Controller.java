6c65a8e336fabeb86dc72790a9ba2ddcece195f2
Gabriel Cruz Libreros <gabrielcl45@gmail.com>
2021-05-25 00:23:13 -0500

    Fix bugs in controller class of main window

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -45,7 +45,44 @@ public class Controller {
     private Label resultLabel;
 
     /**
+     * toma las variables ingresadas por el usuario
+     *
+     * @param event
+     */
+    @FXML
+    void getVariables(ActionEvent event) {
+        //traer información
+        txtFieldVariables.setDisable(true);
+        txtFieldVariables.setText("S," + txtFieldVariables.getText());
+        String[] variables = txtFieldVariables.getText().split(",");
+        butContinueVariables.setDisable(true);
+
+        //asignar los datos recogidos al modelo
+        Variable variableToAdd = new Variable("S", true);
+        g.addVariable(variableToAdd);
+        for (int i = 1; i < variables.length; i++) {
+            variableToAdd = new Variable(variables[i], false);
+            g.addVariable(variableToAdd);
+        }
+
+        //activar ingreso de producciones
+        choiceBoxVariable.setDisable(false);
+        txtFieldProduction.setDisable(false);
+        choiceBoxTipoRegla.setDisable(false);
+        butAddProduction.setDisable(false);
+        butContinuarCadena.setDisable(false);
+
+        //asignar los valores a los combobox
+        choiceBoxVariable.setValue("S");
+        choiceBoxVariable.getItems().addAll(variables);
+        choiceBoxTipoRegla.getItems().add("Simple");
+        choiceBoxTipoRegla.getItems().add("Binaria");
+        choiceBoxTipoRegla.setValue("Simple");
+    }
+
+    /**
      * guarda una producción
+     *
      * @param event
      */
     @FXML
@@ -65,7 +102,9 @@ public class Controller {
                     g.getVariablesOfSimpleProductionRules().add(initialVariable_);
                     break;
                 case ("Binaria"):  // caso para guardar producciones binarias
-                    newRule = new Rule(g.getVariableByName(textProduction.substring(0,1)),g.getVariableByName(textProduction.substring(1,2)));
+                    Variable firstVariable = g.getVariableByName(textProduction.substring(0, 1));
+                    Variable secondVariable = g.getVariableByName(textProduction.substring(1, 2));
+                    newRule = new Rule(firstVariable, secondVariable);
                     initialVariable_.addRule(newRule);
                     g.getVariablesOfBinaryProductionRules().add(initialVariable_);
                     break;
@@ -73,11 +112,11 @@ public class Controller {
         } catch (NullPointerException e) {
 //            throw new NullPointerException("Variable not found in G", e);
         }
-
     }
 
     /**
      * desabilita ingreso de datos de producciones y activar la cadena para evaluar
+     *
      * @param event
      */
     @FXML
@@ -94,15 +133,31 @@ public class Controller {
         butAddProduction.setDisable(true);
 
         //debugging section
-        System.out.println("G:");
-        System.out.println("Simples:");
-        for (Variable variable:g.getVariablesOfSimpleProductionRules()
-             ) {
-            System.out.println(variable.getName());
-        }
-        for (Variable variable:g.getVariablesOfBinaryProductionRules()
-        ) {
-            System.out.println(variable.getName());
+//        System.out.println("G:");
+//        System.out.println("Simples:");
+//        for (Variable variable : g.getVariablesOfSimpleProductionRules()
+//        ) {
+//            System.out.println(variable.getName());
+//        }
+//        for (Variable variable : g.getVariablesOfBinaryProductionRules()
+//        ) {
+//            System.out.println(variable.getName());
+//        }
+    }
+
+    /**
+     * evalua la cadena usando el algortmo den modelo
+     *
+     * @param event
+     */
+    @FXML
+    void evaluarCadena(ActionEvent event) {
+        resultLabel.setVisible(true);
+        boolean canBeProduced = CYK.belongsToCFG(g, txtCadena.getText());
+        if (canBeProduced) {
+            resultLabel.setText("G produce w.");
+        } else {
+            resultLabel.setText("G no produce w.");
         }
     }
 
@@ -112,17 +167,20 @@ public class Controller {
     public static void test() {
         //Debugging area for the CYK algorithm.
         CFG testG = new CFG("Test G");
-        Variable s = new Variable("S",true);
-        Variable a = new Variable("A",false);
-        Variable b = new Variable("B",false);
-        Variable c = new Variable("C",false);
-        s.addRule(new Rule(b,a));
-        s.addRule(new Rule(a,c));
-        a.addRule(new Rule(c,c));
+        Variable s = new Variable("S", true);
+        Variable a = new Variable("A", false);
+        Variable b = new Variable("B", false);
+        Variable c = new Variable("C", false);
+        s.addRule(new Rule(b, a));
+        s.addRule(new Rule(a, c));
+
+        a.addRule(new Rule(c, c));
         a.addRule(new Rule("b"));
-        b.addRule(new Rule(a,b));
+
+        b.addRule(new Rule(a, b));
         b.addRule(new Rule("a"));
-        c.addRule(new Rule(b,a));
+
+        c.addRule(new Rule(b, a));
         c.addRule(new Rule("a"));
 
         testG.getVariablesOfSimpleProductionRules().add(a);
@@ -134,58 +192,10 @@ public class Controller {
         testG.getVariablesOfBinaryProductionRules().add(b);
         testG.getVariablesOfBinaryProductionRules().add(c);
 
-        // All test cases should return true.
-        System.out.println(CYK.belongsToCFG(testG,"bbab"));
-        System.out.println(CYK.belongsToCFG(testG,"baaba"));
-        System.out.println(CYK.belongsToCFG(testG,"aaba"));
-    }
-
-    /**
-     * evalua la cadena usando el algortmo den modelo
-     * @param event
-     */
-    @FXML
-    void evaluarCadena(ActionEvent event) {
-        resultLabel.setVisible(true);
-        boolean canBeProduced = CYK.belongsToCFG(g, txtCadena.getText());
-        if (canBeProduced) {
-            resultLabel.setText("G produce la cadena w.");
-        } else {
-            resultLabel.setText("G no produce la cadena w.");
-        }
-    }
-
-    /**
-     * toma las variables ingresadas por el usuario
-     * @param event
-     */
-    @FXML
-    void getVariables(ActionEvent event) {
-        //traer información
-        txtFieldVariables.setDisable(true);
-        txtFieldVariables.setText("S," + txtFieldVariables.getText());
-        String[] variables = txtFieldVariables.getText().split(",");
-        butContinueVariables.setDisable(true);
-
-        //asignar los datos recogidos al modelo
-        Variable variableToAdd = new Variable("S",true);
-        for (int i = 1; i < variables.length; i++) {
-            variableToAdd = new Variable(variables[i],false);
-            g.addVariable(variableToAdd);
-        }
-
-        //activar ingreso de producciones
-        choiceBoxVariable.setDisable(false);
-        txtFieldProduction.setDisable(false);
-        choiceBoxTipoRegla.setDisable(false);
-        butAddProduction.setDisable(false);
-        butContinuarCadena.setDisable(false);
-
-        //asignar los valores a los combobox
-        choiceBoxVariable.setValue("S");
-        choiceBoxVariable.getItems().addAll(variables);
-        choiceBoxTipoRegla.getItems().add("Simple");
-        choiceBoxTipoRegla.getItems().add("Binaria");
-        choiceBoxTipoRegla.setValue("Simple");
+        // All test cases should return true, except the last.
+        System.out.println(CYK.belongsToCFG(testG, "bbab"));
+        System.out.println(CYK.belongsToCFG(testG, "baaba"));
+        System.out.println(CYK.belongsToCFG(testG, "aaba"));
+        System.out.println(CYK.belongsToCFG(testG, "bbbaabb"));
     }
 }
